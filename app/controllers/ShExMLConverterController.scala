9d4086fae897bb32e91e313f461a366491ff280f
Herminio García González <herminiogg@hotmail.com>
2022-10-07 14:36:40 +0200

    First commit with the MVP

---
+++ app/controllers/ShExMLConverterController.scala
@@ -0,0 +1,70 @@
+package controllers
+
+import com.herminiogarcia.shexml.MappingLauncher
+import com.typesafe.config.Config
+import play.api.Logging
+
+import javax.inject._
+import play.api.mvc._
+
+import java.net.URL
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
+@Singleton
+class ShExMLConverterController @Inject()(val controllerComponents: ControllerComponents, config: Config) extends BaseController with Logging {
+
+  def convertRepository(id: String) = Action { implicit request: Request[AnyContent] =>
+    logger.info("Requested repository conversion of " + id)
+    val rules = getMappingRules("./conf/mappingRules/institutions.shexml")
+    val finalMappingRules = getFinalMappingRules("repository", rules, id)
+    handleAnswerWithContentNegotiation(finalMappingRules)
+  }
+
+  def convertUnit(id: String) = Action { implicit request: Request[AnyContent] =>
+    logger.info("Requested unit conversion of " + id)
+    val rules = getMappingRules("./conf/mappingRules/units.shexml")
+    val finalMappingRules = getFinalMappingRules("unit", rules, id)
+    handleAnswerWithContentNegotiation(finalMappingRules)
+  }
+
+  private def handleAnswerWithContentNegotiation(mappingRules: String)(implicit request: Request[AnyContent]): Result = {
+    val AcceptsRDFXML = Accepting("application/rdf+xml")
+    val AcceptsNTriples = Accepting("application/n-triples")
+    val AcceptsJsonLD = Accepting("application/ld+json")
+    val AcceptsTurtle = Accepting("text/turtle")
+    render {
+      case AcceptsRDFXML() => handleMapping(mappingRules, "RDF/XML")
+      case AcceptsNTriples() => handleMapping(mappingRules, "N-Triples")
+      case AcceptsJsonLD() => handleMapping(mappingRules, "JSON-LD")
+      case AcceptsTurtle() => handleMapping(mappingRules, "Turtle")
+      case _ => handleMapping(mappingRules, request.getQueryString("format").getOrElse("RDF/XML"))
+    }
+  }
+
+  private def handleMapping(mappingRules: String, format: String): Result = {
+    Try(new MappingLauncher().launchMapping(mappingRules, format)) match {
+      case Success(result) => Ok(result)
+      case Failure(exception) => {
+        logger.error("Error while translating to RDF ", exception)
+        InternalServerError("Something went wrong!")
+      }
+    }
+  }
+
+  private def getFinalMappingRules(theType: String, rules: String, id: String): String = {
+    val portalURL = new URL(config.getString("ehri.shexml.converter.portal.url")) // needs to be parametrised
+    val apiSource = if(theType.toLowerCase == "repository")
+        s"SOURCE repositories <${portalURL.getProtocol}://${portalURL.getHost}:${portalURL.getPort}/api/v1/$id>"
+      else s"SOURCE holdings <${portalURL.getProtocol}://${portalURL.getHost}:${portalURL.getPort}/api/v1/$id>"
+    val termsSource = s"SOURCE terms <${portalURL.getProtocol}://${portalURL.getHost}:${portalURL.getPort}/units/$id/export?asFile=true>"
+    rules
+      .replaceFirst("SOURCE (repositories|holdings) <placeholder>", apiSource)
+      .replaceFirst("SOURCE terms <placeholder>", termsSource)
+  }
+
+  private def getMappingRules(path: String): String = {
+    val file = Source.fromFile(path)
+    try { file.iterator.mkString } finally { file.close() }
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,21 @@
+name := """ehri-shexml-converter-ws"""
+organization := "eu.ehri-project"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.13.9"
+
+libraryDependencies += guice
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "5.0.0" % Test
+libraryDependencies += "com.herminiogarcia" %% "shexml" % "0.3.1"
+
+dependencyOverrides += "org.scala-lang.modules" %% "scala-parser-combinators" % "2.1.1"
+dependencyOverrides += "com.fasterxml.jackson.core" % "jackson-databind" % "2.11.4"
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "eu.ehri-project.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "eu.ehri-project.binders._"
